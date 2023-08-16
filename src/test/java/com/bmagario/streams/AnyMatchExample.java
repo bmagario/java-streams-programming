b2dc61a1769b6008108391342848b98bfd82cb19
bmagario <brian.magario@gmail.com>
2023-08-16 15:55:39 -0300

    AnyMatch Example

---
+++ src/test/java/com/bmagario/streams/AnyMatchExample.java
@@ -0,0 +1,21 @@
+package com.bmagario.streams;
+
+import com.bmagario.models.JsonMockData;
+import com.bmagario.models.Sommelier;
+import com.bmagario.models.Wine;
+import java.io.IOException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class AnyMatchExample {
+
+    @Test
+    void anyMatch() throws IOException {
+        List<Sommelier> sommeliers = JsonMockData.getSommeliers();
+
+        boolean hasRedWineExpertise = sommeliers.stream()
+                .anyMatch(sommelier -> sommelier.getExpertise().equals("Red wines"));
+
+        System.out.println("Does any sommelier specialize in red wines? " + hasRedWineExpertise);
+    }
+}
