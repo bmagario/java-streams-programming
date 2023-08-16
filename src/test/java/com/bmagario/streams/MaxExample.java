18af00a64e425a9a4f5cfced987d0097f29a8c58
bmagario <brian.magario@gmail.com>
2023-08-16 16:12:42 -0300

    Max Example

---
+++ src/test/java/com/bmagario/streams/MaxExample.java
@@ -0,0 +1,23 @@
+package com.bmagario.streams;
+
+import com.bmagario.models.JsonMockData;
+import com.bmagario.models.Sommelier;
+import com.bmagario.models.Wine;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public class MaxExample {
+
+    @Test
+    void max() throws IOException {
+        List<Wine> wines = JsonMockData.getWines();
+
+        Optional<Wine> mostExpensiveWine = wines.stream()
+                .max(Comparator.comparingDouble(wine -> wine.getPrice()));
+
+        System.out.println("The most expensive wine is: " + mostExpensiveWine.get().getName());
+    }
+}
