c07bf5cec2f5ccae53a14175c5cea55714c1093b
bmagario <brian.magario@gmail.com>
2023-08-16 16:17:15 -0300

    Average Example

---
+++ src/test/java/com/bmagario/streams/AverageExample.java
@@ -0,0 +1,23 @@
+package com.bmagario.streams;
+
+import com.bmagario.models.JsonMockData;
+import com.bmagario.models.Sommelier;
+import com.bmagario.models.Wine;
+import java.io.IOException;
+import java.util.List;
+import java.util.OptionalDouble;
+import org.junit.jupiter.api.Test;
+
+public class AverageExample {
+
+    @Test
+    void average() throws IOException {
+        List<Wine> wines = JsonMockData.getWines();
+
+        OptionalDouble averagePriceOfWines = wines.stream()
+                .mapToDouble(Wine::getPrice)
+                .average();
+
+        System.out.println("The average price of all the wines is: " + averagePriceOfWines.getAsDouble());
+    }
+}
