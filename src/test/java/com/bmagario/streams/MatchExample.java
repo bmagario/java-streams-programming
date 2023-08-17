5d84a95e916c04c3e2e4193eced44d65ec191cea
bmagario <brian.magario@gmail.com>
2023-08-17 12:41:34 -0300

    Match Example

---
+++ src/test/java/com/bmagario/streams/MatchExample.java
@@ -0,0 +1,22 @@
+package com.bmagario.streams;
+
+import com.bmagario.models.JsonMockData;
+import com.bmagario.models.Sommelier;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+public class MatchExample {
+
+    @Test
+    void match() throws IOException {
+        List<Sommelier> sommeliers = JsonMockData.getSommeliers();
+
+        boolean allSommeliersSpecializeInRedWines = sommeliers.stream()
+                .allMatch(sommelier -> sommelier.getExpertise().equals("Red wines"));
+
+        System.out.println("Do all sommeliers specialize in red wines? "
+                + allSommeliersSpecializeInRedWines);
+    }
+}
